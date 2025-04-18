0fb4f5fc862eb472a79ac4f36555ed4649982b58
VladMihai23 <vlamivox@yahoo.com>
2025-04-18 20:07:31 +0300

    Football news

--- src/main/java/com/scorezone/scorezone/repository/MatchRepository.java
+++ src/main/java/com/scorezone/scorezone/repository/MatchRepository.java
@@ -4,10 +4,9 @@ import com.scorezone.scorezone.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.List;
+
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
     Optional<Match> findByFixtureId(Long FixtureId);
-    List<Match> findByStatus(String status);
 }
 
